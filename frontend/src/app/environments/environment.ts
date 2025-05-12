4ae76475aef58b8f6ad16ebe43ca6eeab6f59ed9
SkyLightSK <skyalightsk@gmail.com>
2025-05-12 00:32:09 -0500

    revert env

--- frontend/src/app/environments/environment.ts
+++ frontend/src/app/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001/api',
+  apiUrl: 'http://localhost:3000/api',
   authApiPath: '/auth',
   entityApiPath: '/entities',
   deviceApiPath: '/devices',
